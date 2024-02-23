39f3bb132db08c8be6f15998dc3a32733f277425
AlphaZer00 <batudasdemir@gmail.com>
2024-02-23 10:48:11 -0500

    Add function to sort todo list objects by project group

--- src/projectManager.js
+++ src/projectManager.js
@@ -1,3 +1,5 @@
+import {getListItemArr, addListItemToArr, removeListItemFromArr, listItemArr} from './listItemArray';
+
 const projectList = [];
 
 function getProjectList() {
@@ -19,4 +21,26 @@ function deleteProject(project) {
     }
 }
 
-export {getProjectList, addNewProject, deleteProject};
+function sortListItemsByProject() {
+    //create empty object, This object will contain Key:projectGroup, Value: Array of todolist objects that have that projectGroup:
+    const projectObjects = {};
+    //for every project in list of project (i.e 'fitness' or 'work')
+    for (const project of projectList) {
+        //Make Array to hold todolist objects with the matching projectGroup
+        const projectGroupArray = [];
+        //loop through array of all todolist objects
+        for (let i = 0; i < listItemArr.length; i++) {
+            //if todolist object's projectGroup value is equivalent to projectGroup name
+            if (listItemArr[i].projectGroup == project) {
+                //Add that object to the projectGroupArray
+                projectGroupArray.push(listItemArr[i]);
+                //Create key:value pair of project: Array of objects with that projectGroup
+                projectObjects[project] = projectGroupArray;
+            }
+        }
+    }
+    return console.log(projectObjects);
+}
+
+
+export {getProjectList, addNewProject, deleteProject, sortListItemsByProject};
