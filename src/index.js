a9da322f4e2e4ad11bbee346eee10911f349c17f
AlphaZer00 <batudasdemir@gmail.com>
2024-02-27 11:04:40 -0500

    Delete test code and import functions from modules

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import _ from 'lodash';
 import './style.css';
+import createListItem from './listItemFactory';
+import {modifyTitle, modifyDescription, modifyDueDate, modifyPriority, modifyCheckBox, modifyProjectGroup} from './listItemModifier';
+import {getProjectList, addNewProject, deleteProject, sortListItemsByProject} from './projectManager';
+import {getListItemArr, addListItemToArr, removeListItemFromArr, listItemArr} from './listItemArray';
+import displayProjectList from './UI';
 
-function component() {
-    const element = document.createElement('div');
 
-   // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+let todo1 = createListItem('run1', 'run a mile', '2/22/24', "1", false, 'fitness');
+let todo2 = createListItem('run2', 'run a mile', '2/22/24', "1", false, 'work');
+let todo3 = createListItem('run3', 'run a mile', '2/22/24', "1", false, 'home');
+let todo4 = createListItem('run4', 'run a mile', '2/22/24', "1", false, 'home');
 
-    return element;
-}
 
-document.body.appendChild(component());
+addListItemToArr(todo1);
+addListItemToArr(todo2);
+addListItemToArr(todo3);
+addListItemToArr(todo4);
+
+addNewProject('fitness');
+addNewProject('work');
+addNewProject('home');
+sortListItemsByProject();
+
+displayProjectList();
