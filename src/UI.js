3b6b070bfb0c681b23fb3655203700dd6aa2b6fa
AlphaZer00 <batudasdemir@gmail.com>
2024-02-29 09:14:00 -0500

    Create function that renders todoItems to DOM

--- src/UI.js
+++ src/UI.js
@@ -3,24 +3,47 @@ import {getProjectList, addNewProject, deleteProject, sortListItemsByProject} fr
 import {getListItemArr, addListItemToArr, removeListItemFromArr, listItemArr} from './listItemArray';
 
 function displayProjectList() {
+    //copy array from projectList
     const arr = getProjectList();
+    //create new div and add class
     const projectArea = document.createElement('div');
     projectArea.classList.add('project-area');
 
+    //for every project within the array
     for (const project of arr) {
+        //create a div
         const projectDiv = document.createElement('div');
+        //Set div text to project name
         projectDiv.innerText = project;
-        projectDiv.classList.add('project-section');
+        //add class
+        projectDiv.classList.add('project-section', `${project}`);
+        //append div to project container div
         projectArea.append(projectDiv);
     }
-
+    //append project div to html body
     document.body.append(projectArea);
 }
 
-//show Todolist items inside the project-section divs
-function displayProjectItems() {
-    const projectArea = document.querySelector('project-area');
-    projectArea
+function renderItemToDom(obj, project) {
+    const projectArea = document.querySelector('.project-area');
+    const parent = projectArea.querySelector(`.${project}`);
+
+    const itemDiv = document.createElement('div');
+    itemDiv.classList.add('todo-item');
+
+    const title = document.createElement('div');
+    const desc = document.createElement('div');
+    const dueDate = document.createElement('div');
+    const priority = document.createElement('div');
+    const checkBox = document.createElement('div');
+    
+    title.textContent = obj.title;
+    desc.textContent = obj.description;
+    dueDate.textContent = obj.dueDate;
+    priority.textContent = obj.priority;
+    checkBox.textContent = obj.checkBox;
+
+    parent.append(title, desc, dueDate, priority, checkBox);
 }
 
-export default displayProjectList;
+export {displayProjectList, renderItemToDom};
