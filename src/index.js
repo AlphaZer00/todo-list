dd4706a613d33672c55569dbd395b45d00fd8c31
AlphaZer00 <batudasdemir@gmail.com>
2024-04-03 13:08:19 -0400

    Add function calls for event listeners to apply

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
 import _ from 'lodash';
 import './style.css';
 import createListItem from './listItemFactory';
-import {modifyTitle, modifyDescription, modifyDueDate, modifyPriority, modifyCheckBox, modifyProjectGroup} from './listItemModifier';
 import {getProjectList, addNewProject, deleteProject, sortListItemsByProject} from './projectManager';
-import {getListItemArr, addListItemToArr, removeListItemFromArr, listItemArr} from './listItemArray';
-import displayProjectList from './UI';
-
-
-let todo1 = createListItem('run1', 'run a mile', '2/22/24', "1", false, 'fitness');
-let todo2 = createListItem('run2', 'run a mile', '2/22/24', "1", false, 'work');
-let todo3 = createListItem('run3', 'run a mile', '2/22/24', "1", false, 'home');
-let todo4 = createListItem('run4', 'run a mile', '2/22/24', "1", false, 'home');
-
-
-addListItemToArr(todo1);
-addListItemToArr(todo2);
-addListItemToArr(todo3);
-addListItemToArr(todo4);
+import {getListItemArr, addListItemToArr, removeListItemFromArr, addUniqueID, listItemArr} from './listItemArray';
+import {displayProjectList, setModalProjectSelectors, handleModalButtons, createListItemFromFormInput, loadEditModalValues, updateListItemFromFormInput, loadItemsFromStorage, handleAddProjectModalButtons, createProjectFromForm, handleRemoveProjectModalButtons, handleProjectDeleteButton} from './UI';
+import { storeObj, removeObjFromStorage, getKeyArrFromStorage } from './storage';
 
 addNewProject('fitness');
 addNewProject('work');
 addNewProject('home');
+
+
+handleModalButtons();
+handleRemoveProjectModalButtons();
+handleAddProjectModalButtons();
+handleProjectDeleteButton();
+
 sortListItemsByProject();
 
-displayProjectList();
+setModalProjectSelectors();
+
+createListItemFromFormInput();
+updateListItemFromFormInput();
+
+createProjectFromForm();
+
+window.addEventListener('load', () => loadItemsFromStorage());
