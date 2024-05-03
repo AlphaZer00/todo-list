6dd33de747d855a7e41593bd6f1e0f897655ab44
AlphaZer00 <batudasdemir@gmail.com>
2024-05-02 20:14:29 -0400

    Add linter + prettier and reformat code

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: 'production',
+  devtool: 'source-map',
   devServer: {
-    static: './src',
+    static: './dist',
   },
   output: {
     filename: 'bundle.js',
@@ -17,15 +20,31 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|svg)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext]'
+        }
+      },
+      {
+        test: /\.html$/i,
+        use: [
+          {
+            loader: "html-loader",
+          }
+        ]
       },
     ],
   },
-  plugins: [
+  plugins:
+  [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
     new HtmlWebpackPlugin({
         filename: 'index.html',
         template: './src/index.html',
@@ -33,6 +52,11 @@ module.exports = {
         inject: 'head',
         scriptLoading: 'defer',
     }),
-    new FaviconsWebpackPlugin('src/assets/favicon.svg')
-  ]
+    new FaviconsWebpackPlugin('src/assets/favicon.svg'),
+  ],
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ],
+  },
 };
